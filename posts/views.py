eddeb6a0a9750d6079f3276833df861d83acaa8b
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2018-10-16 10:00:06 +0600

    post detail view

--- posts/views.py
+++ posts/views.py
@@ -23,3 +23,31 @@ class PostList(SelectRelatedMixin, generic.ListView):
     select_related = ("user", "group")
 
 
+class UserPost(generic.ListView):
+    model = models.Post
+    template_name = "posts/user_post_list.html"
+
+    def get_queryset(self):
+        try:
+            self.post_user = User.objects.prefetch_related("post").get(username__iexact=self.kwargs.get("username"))
+
+        except User.DoesNotExist:
+            raise Http404
+        else:
+            self.post_user.posts.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["post_user"] = self.post_user
+        return context
+
+
+class PostDetail(SelectRelatedMixin, generic.DetailView):
+    model = models.Post
+    select_related = ("user", "group")
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(
+            user__username__iexact=self.kwargs.get("username")
+        )
